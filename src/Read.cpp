9e4a24bda8df5646e349825ce4719cdcfde5ba82
Alessio Sclocco <alessio@sclocco.eu>
2014-04-04 15:06:40 -0700

    Damn my forgetful mind.

--- src/Read.cpp
+++ src/Read.cpp
@@ -123,7 +123,7 @@ int main(int argc, char * argv[]) {
 				read.generateCode();
 
 				B->copyHostToDevice(true);
-				read(B);
+				read(B, C);
 				(read.getTimer()).reset();
 				for ( unsigned int iter = 0; iter < nrIterations; iter++ ) {
 					read(B, C);
