26c4e755606a595f0acc778a4654f23b80f4fc0c
Alessio Sclocco <alessio@sclocco.eu>
2014-09-15 17:03:35 +0200

    Adding an asymmetric flops benchmark.

--- include/Flops.hpp
+++ include/Flops.hpp
@@ -24,6 +24,7 @@ namespace isa {
 namespace Benchmarks {
 
 std::string  * getFlopsOpenCL(const unsigned int iterations, const unsigned int vector, const std::string & dataType);
+std::string  * getFlopsOpenCL4x2(const unsigned int iterations, const std::string & dataType);
 
 
 // Implementations
@@ -70,6 +71,43 @@ std::string * getFlopsOpenCL(const unsigned int iterations, const unsigned int v
   return code;
 }
 
+std::string * getFlopsOpenCL4x2(const unsigned int iterations, const std::string & dataType) {
+  std::string * code = new std::string();
+
+  *code = "__kernel void flops(__global const " + dataType + " * restrict const A, __global const " + dataType + " * restrict const B, __global " + dataType + " * C) {\n"
+    + dataType + "4 temp;\n"
+    + dataType + "2 a_0;\n"
+    + dataType + "2 a_1;\n"
+    + dataType + "2 b_0;\n"
+    + dataType + "2 b_1;\n"
+    + dataType + "2 c_0;\n"
+    + dataType + "2 c_1;\n"
+    // Load
+    "temp = vload" + "4(0, &(A[get_global_id(0) * " + isa::utils::toString(vector) + "]));\n"
+    "a_0 = (" + dataType + "2)(temp.s0, temp.s1);\n"
+    "a_1 = (" + dataType + "2)(temp.s2, temp.s3);\n"
+    "temp = vload" + "4(0, &(B[get_global_id(0) * " + isa::utils::toString(vector) + "]));\n"
+    "b_0 = (" + dataType + "2)(temp.s0, temp.s1);\n"
+    "b_1 = (" + dataType + "2)(temp.s2, temp.s3);\n"
+    // Compute
+    "<%COMPUTE%>"
+    // Store
+    "temp = (" + dataType + "4)(c_0.s0, c_0.s1, c_1.s0, c_1.s1);\n"
+    "vstore" + "4(temp, 0, &(C[get_global_id(0) * " + isa::utils::toString(vector) + "]));\n"
+    "}\n";
+  std::string computeTemplate = "c_0 = (a_0 + b_0) * get_local_id(0);\n"
+    "c_1 = (a_1 + b_1) * get_local_id(0);\n";
+
+  std::string * compute_s = new std::string();
+
+  for ( unsigned int i = 0; i < iterations; i++ ) {
+    compute_s->append(computeTemplate);
+  }
+
+  code = isa::utils::replace(code, "<%COMPUTE%>", *compute_s, true);
+
+  return code;
+}
 } // Benchmarks
 } // isa
 
